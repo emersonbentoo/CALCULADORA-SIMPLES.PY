f8f0c8137fb0b386c139c44ef2f8f29ea0fc2073
emersonbentoo <emersonbentoo@outlook.com>
2024-12-19 15:09:45 -0300

    Melhoria do codigo v1.2

--- calculadora.py
+++ calculadora.py
@@ -3,24 +3,26 @@ while True:
     try:
         a = int (input("Digite um valor:"))
         b = int (input("Digite o segundo valor:"))
-        o = int (input("Aperte:\n1 SOMAR\n2 SUBTRAIR\n3 MULTIPLICAR\n4 DIVIDIR\n5 SAIR\n"+"-"*50 +'\n'))
-        if o == 1:
-            t = (a+b)
-            print (f"Total da Soma é {t}")
-        elif o == 2:
-            t = (a-b)
-            print(f"Total da Subtração é {t}")
-        elif o == 3:
-            t = (a*b)
-            print(f"valor da multiplicação é {t}")
-        elif o == 4:
-            
-            t = (a/b)
-            print(f"Total da divisao é {t}")
-        elif o == 5:
-            print("Obrigado, Volte sempre! ")
-            break
-        else:
-            print("operação invalida! tente novamente.")
+        while True:
+            o = int (input("Aperte:\n1 SOMAR\n2 SUBTRAIR\n3 MULTIPLICAR\n4 DIVIDIR\n5 SAIR\n"+"-"*50 +'\n'))
+            if o == 1:
+                t = (a+b)
+                print (f"Total da Soma é {t}")
+            elif o == 2:
+                t = (a-b)
+                print(f"Total da Subtração é {t}")
+            elif o == 3:
+                t = (a*b)
+                print(f"valor da multiplicação é {t}")
+            elif o == 4:
+                if b != 0:
+                    t = (a/b)
+                print(f"Total da divisao é {t}")
+                print("Erro: Divisao por zero nao é permitido!")
+            elif o == 5:
+                print("Obrigado, Volte sempre! ")
+                exit()
+            else:
+                print("operação invalida!.\nRepita a operação desejada")
     except ValueError:
-            print("Operação invalida!, por favor insira apenas numeros validos")
+        print("Operação invalida!, por favor insira apenas numeros validos")
